d61c7362792d48972c1c70542ab9df6493d8aa47
dariameleshina <dariameleschina@yandex.ru>
2022-04-11 19:48:30 +0400

    Нужно больше функций. Модуль 2.

---
+++ js/main.js
@@ -0,0 +1,16 @@
+//Функция, возвращающая случайное целое число из переданного диапазона включительно.
+function randomInteger(min, max) {
+  let rand = min + Math.random() * (max + 1 - min);
+  return Math.floor(rand);
+}
+
+//Функция для проверки максимальной длины строки.
+function checkWritelineLength(currentString, maxLength) {
+  let maxLength <= 140;
+  if (currentString = maxLength) {
+    console.log(true);
+
+  } else {
+   console.log(false);
+  }
+}
